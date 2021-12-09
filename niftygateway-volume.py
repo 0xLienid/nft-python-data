474ac9fc272a0374e9bf767b5b7bf7b34d6b1856
Lienid <0xLienid@protonmail.com>
2021-12-08 23:29:56 -0500

    new version getting around reate limits by being slow as hell

--- niftygateway-volume.py
+++ niftygateway-volume.py
@@ -9,7 +9,7 @@ import time
 def main():
     tracker = NiftyGatewayVolumeTracker()
 
-    rows = Parallel(n_jobs=50)(delayed(tracker.get_trades)(i) for i in range(1, 5000))
+    rows = [tracker.get_trades(i) for i in range(1, 1001)]
     trades = pd.DataFrame()
     for row in rows:
         trades = pd.concat([trades, pd.DataFrame(row)])
@@ -18,6 +18,7 @@ def main():
     trades["tx_amount"] = (trades["birthingPurchaseAmountInCents"] + trades["saleAmountInCents"]) / 100
 
     weekly_volume = tracker.calc_weekly_volume(trades)
+    weekly_volume.sort_values('timestamp')
     print(weekly_volume)
 
     plt.figure(1)
@@ -26,7 +27,7 @@ def main():
     ax.bar(weekly_volume.index, weekly_volume["tx_amount"])
     plt.xlabel("Date")
     plt.ylabel("$ Volume")
-    plt.title("NiftyGateway Daily NFT Volume")
+    plt.title("NiftyGateway Weekly NFT Volume")
 
     fig.autofmt_xdate()
 
@@ -38,29 +39,28 @@ class NiftyGatewayVolumeTracker():
         self.url = "https://api.niftygateway.com/market/all-data/?page=%7B%22current%22:{},%22size%22:100%7D&filters=%7B%22exclude_types%22:[%22withdrawals%22,%22single_nifty_offers%22,%22global_nifty_offers%22,%22attribute_nifty_offers%22,%22primary_market_bids%22,%22gifts%22,%22deposits%22,%22listings%22]%7D"
 
     def get_trades(self, i):
-        ids = set()
-        all_trades = []
+        print(i)
+        trades = []
 
-        for i in range(100):
-            try:
-                r = requests.get(self.url.format(i))
-                data = r.json()
+        for j in range(100):
+            print(j)
+            r = requests.get(self.url.format(i))
+            data = r.json()
+            #print(data)
+            print(data['message'])
 
-                if data['details'].index('limit') > -1:
-                    time.sleep(5)
-                    continue
-                else:
-                    try:
-                        trades = data['data']['results']
-                        trades = [{'timestamp': t['Timestamp'], 'type': t['Type'], 'id': t['id'], 'birthingPurchaseAmountInCents': t['BirthingPurchaseAmountInCents'], 'saleAmountInCents': t['SaleAmountInCents']} for t in trades]
-                    except:
-                        trades = data
-                        trades = [{'timestamp': t['Timestamp'], 'type': t['Type'], 'id': t['id'], 'birthingPurchaseAmountInCents': t['BirthingPurchaseAmountInCents'], 'saleAmountInCents': t['SaleAmountInCents']} for t in trades]
-                    
-                    break
-            except:
+            if 'limit' in data['message']:
                 time.sleep(5)
                 continue
+            else:
+                try:
+                    trades = data['data']['results']
+                    trades = [{'timestamp': t['Timestamp'], 'type': t['Type'], 'id': t['id'], 'birthingPurchaseAmountInCents': t['BirthingPurchaseAmountInCents'], 'saleAmountInCents': t['SaleAmountInCents']} for t in trades]
+                    break
+                except:
+                    trades = data
+                    trades = [{'timestamp': t['Timestamp'], 'type': t['Type'], 'id': t['id'], 'birthingPurchaseAmountInCents': t['BirthingPurchaseAmountInCents'], 'saleAmountInCents': t['SaleAmountInCents']} for t in trades]
+                    break
 
         return trades
 
